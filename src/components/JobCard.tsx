ac513dfb030cd4e03d9501432845fe3951221aac
rezinsaleem <rezinsaleem1@gmail.com>
2025-05-29 18:37:53 +0530

    job logo

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -54,7 +54,7 @@ export function JobCard({ job }: { job: Job }) {
           <div className="bg-gradient-to-t from-[#f1f1f1] to-white p-3 border-2 border-white rounded-xl shadow-[0_2px_4px_rgba(0,0,0,0.05)]">
             <div className="rounded-full w-12 h-12 flex items-center justify-center">
                 <Image
-                  src="/logo.svg"
+                  src="/nick.png"
                   alt="Amazon Logo"
                   width={30}
                   height={30}
